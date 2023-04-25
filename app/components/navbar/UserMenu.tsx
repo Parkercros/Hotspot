085b19580e518f8b6a06db6815a19e6cfb0cc9c4
Parker Crosgrove <pdxparker3@gmail.com>
2023-04-25 14:12:57 -0600

    Navigation UI complete

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,16 +1,22 @@
-'use client';
+"use client";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import MenuItem  from "./MenuItem";
 
 const UserMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return ( 
-        <div className="relative">      
-            <div className="flex flex-row items-center gap-3">
-                <div
-                onClick={() => {}}
-                className="
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="
                 hidden
                 md:block
                 text-sm
@@ -22,13 +28,12 @@ const UserMenu = () => {
                 transition
                 cursor-pointer  
                 "
-                
-                >
-                Airbnb your home    
-                </div>
-                <div
-                onClick={() => {}}
-                className="
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={toggleOpen}
+          className="
                 p-4
                 md:py-1
                 md:px-2
@@ -43,15 +48,43 @@ const UserMenu = () => {
                 hover:shadow-md
                 transition
                 "
-                >
-                    <AiOutlineMenu />
-                    <div className="hidden md:block">
-                    <Avatar />
-                    </div>
-                </div>
-            </div>
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
-     );
-}
+      </div>
 
+      {isOpen && (
+        <div
+          className="
+                absolute
+                rounded-xl
+                shadow-md
+                w-[40vw]
+                md:w-3/4
+                bg-white
+                overflow-hidden
+                right-0
+                top-12
+                text-sm
+                "
+        >
+            <div className="flex flex-col cursor-pointer"></div>
+            <> 
+            <MenuItem 
+            onClick={() => {}}
+            label="Login"
+            />
+            <MenuItem 
+            onClick={() => {}}
+            label="Sign up"
+            />
+            </>
+        </div>
+      )}
+    </div>
+  );
+};
 export default UserMenu;
