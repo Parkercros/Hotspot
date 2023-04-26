b6fb2f947790fc4427d423c34621390c7a8b404a
Parker Crosgrove <pdxparker3@gmail.com>
2023-04-25 20:38:50 -0600

    finished register UI (no backend yet)

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,6 +9,8 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal= () => {
     const registerModal = useRegisterModal();
@@ -38,7 +40,7 @@ const RegisterModal= () => {
           })
 
         .catch((err) => {
-            console.log(err);
+            toast.error('Something went wrong')
         })
         .finally(() => {
             setIsLoading(false);
@@ -69,11 +71,55 @@ const RegisterModal= () => {
                         <Input
             id="Password"
             label="Password"
+            type="password"
             disabled={isLoading}
             register={register}
             errors={errors}
             required
             />
+            </div>
+    )
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+            outline
+            label="Continue with Google"
+            icon={FcGoogle}
+            onClick={() => {}}
+            />
+            <Button
+            outline
+            label="Continue with Github"
+            icon={AiFillGithub}
+            onClick={() => {}}
+            />
+            <div
+            className="
+            text-neutral-500
+            text-center
+            mt-4
+            font-light
+            "
+            >
+            <div className="
+            justify-center flex flex-row items-center gap-2">
+                <div>
+                Already have an account?
+                </div>
+                <div
+                 onClick={registerModal.onClose}
+                className="
+                text-neutral-800
+                cursor-pointer
+                hover:underline
+                "
+                >
+                Log in
+                </div>
+            </div>
+            </div>
             </div>
     )
 
@@ -87,6 +133,7 @@ const RegisterModal= () => {
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
         body={bodyContent}
+        footer={footerContent}
 
         />
      );
