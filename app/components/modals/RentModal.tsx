ab3887fff717714b9255988e5fe25006f64c6ad9
Parker Crosgrove <pdxparker3@gmail.com>
2023-04-27 18:41:25 -0600

    Category input UI complete

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -3,6 +3,8 @@ import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
 import Heading from '../Heading';
 import { useMemo, useState } from "react";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 
 enum STEPS {
@@ -50,12 +52,35 @@ return undefined;
 }, [step]);
 
 let bodyContent = (
-<div className="flex felx-col gap-8">
-    <Heading
-    title="Which of these best describes your property?"
-    subtitle= "Pick a category"
-    />
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+      <div 
+        className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
+        "
+      >
+{categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+        <CategoryInput
+        onClick = {() => {}}
+        selected = {false}
+        label = {item.label}
+        icon = {item.icon}
 
+        />
+
+    </div>
+
+))}
+</div>
 </div>
 
 
