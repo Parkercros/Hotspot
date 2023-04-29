3a5001977cd86cbe38b158dfe6eaac72d9070f7e
Parker Crosgrove <pdxparker3@gmail.com>
2023-04-29 15:57:08 -0600

    remove all filters button added

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,27 @@ export default function Home() {
   if (isEmpty) {
     return (
       <ClientOnly>
-        <EmptyState />
+        <EmptyState showReset />
       </ClientOnly>
-    );
+    )
   }
 
   return (
     <ClientOnly>
+      <EmptyState />
       <Container>
         <div
           className="
-          pt-24
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8"
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
         >
           <div>Listings</div>
         </div>
