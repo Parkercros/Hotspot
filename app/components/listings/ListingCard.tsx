937bd1cd68f2539efda5258931ea47471d35d073
Parker Crosgrove <pdxparker3@gmail.com>
2023-04-30 18:31:20 -0600

    Favoriting aspect finally works

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,22 +1,30 @@
 'use client';
 
-import { Listing, Reservation } from "@prisma/client";
-import { useRouter } from "next/navigation";
-import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
-import React, { useCallback, useMemo } from "react";
-import { format } from "date-fns";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { format } from 'date-fns';
+
+import useCountries from "@/app/hooks/useCountries";
+import { 
+  SafeListing, 
+  SafeReservation, 
+  SafeUser 
+} from "@/app/types";
+
+import HeartButton from "../HeartButton";
+import Button from "../Button";
+import ClientOnly from "../ClientOnly";
 
 interface ListingCardProps {
-  data: Listing;
-  reservation?: Reservation
+  data: SafeListing;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
   actionId?: string;
-  currentUser?: SafeUser | null;
-}
+  currentUser?: SafeUser | null
+};
 
 const ListingCard: React.FC<ListingCardProps> = ({
   data,
@@ -29,18 +37,19 @@ const ListingCard: React.FC<ListingCardProps> = ({
 }) => {
   const router = useRouter();
   const { getByValue } = useCountries();
+
   const location = getByValue(data.locationValue);
 
   const handleCancel = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.stopPropagation();
+    e.stopPropagation();
 
-      if (disabled) {
-        return;
-      }
+    if (disabled) {
+      return;
+    }
 
-      onAction?.(actionId)
-    }, [disabled, onAction, actionId]);
+    onAction?.(actionId)
+  }, [disabled, onAction, actionId]);
 
   const price = useMemo(() => {
     if (reservation) {
@@ -61,40 +70,69 @@ const ListingCard: React.FC<ListingCardProps> = ({
     return `${format(start, 'PP')} - ${format(end, 'PP')}`;
   }, [reservation]);
 
-  return ( 
-    <div
-    onClick={() => router.push(`/listings/${data.id}`)}
-    className="
-    col-span-1 cursor-pointer group
-    "
+  return (
+    <div 
+      onClick={() => router.push(`/listings/${data.id}`)} 
+      className="col-span-1 cursor-pointer group"
     >
-      <div className="flex flex-col gap-2 w-full"
+      <div className="flex flex-col gap-2 w-full">
+        <div 
+          className="
+            aspect-square 
+            w-full 
+            relative 
+            overflow-hidden 
+            rounded-xl
+          "
         >
-<div
-className="
-aspect-square 
-w-full 
-relative 
-overflow-hidden 
-rounded-xl
-"
->
-    <Image
-    fill
-    alt= "Listing"
-    src={data.imageSrc}
-    className='
-    object-cover
-    h-full
-    w-full
-    group-hover:scale-110
-    transition'
-    />
-</div>
-
+          <Image
+            fill
+            className="
+              object-cover 
+              h-full 
+              w-full 
+              group-hover:scale-110 
+              transition
+            "
+            src={data.imageSrc}
+            alt="Listing"
+          />
+          <div className="
+            absolute
+            top-3
+            right-3
+          ">
+            <HeartButton 
+              listingId={data.id} 
+              currentUser={currentUser}
+            />
+          </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">
+            $ {price}
+          </div>
+          {!reservation && (
+            <div className="font-light">night</div>
+          )}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel} 
+            onClick={handleCancel}
+          />
+        )}
+      </div>
     </div>
-  );
+   );
 }
-
+ 
 export default ListingCard;
