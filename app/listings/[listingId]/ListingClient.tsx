0a1d6d682ee9e0dda393ca040fe55a86f672f5be
Parker Crosgrove <pdxparker3@gmail.com>
2023-04-30 19:40:06 -0600

    easy fix. images are now on listing pages

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -9,39 +9,35 @@ import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 
 interface ListingClientProps {
-reservations?: Reservation[];
-listing: SafeListing & {
-    user: SafeUser
-};
-currentUser?: SafeUser | null;
+  reservations?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+    imageSrc: string; 
+  };
+  currentUser?: SafeUser | null;
 }
 
-const ListingClient: React.FC<ListingClientProps>  = ({
-listing,
-currentUser,
-
+const ListingClient: React.FC<ListingClientProps> = ({
+  listing,
+  currentUser,
 }) => {
-    const category = useMemo(() => {
-        return categories.find((item) =>
-        item.label === listing.category);
-    }, [ listing.category]);
-    return ( 
+  const category = useMemo(() => {
+    return categories.find((item) => item.label === listing.category);
+  }, [listing.category]);
+  
+  return (
+    <Container>
+      <div className="flex flex-col gap-6">
+        <ListingHead
+          title={listing.title}
+          imageSrc={listing.imageSrc} 
+          locationValue={listing.locationValue}
+          id={listing.id}
+          currentUser={currentUser}
+        />
+      </div>
+    </Container>
+  );
+};
 
-        <Container>
-            <div className="max-w-screen-lg mx-auto">
-                <div className="flex flex-col gap-6">
-                    <ListingHead
-                    title={listing.title}
-                    imageSrc-={listing.imageSrc}
-                    locationValue={listing.locationValue}
-                    id = {listing.id}
-                    currentUser = {currentUser}
-                    />
-                </div>
-
-                </div>
-        </Container>
-     );
-}
- 
-export default ListingClient ;
+export default ListingClient;
